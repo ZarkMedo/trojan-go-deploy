ae97615013537b0d43e3a2c74c33a4c94505e65d
Medo <medo@medo.com>
2024-01-15 14:16:17 +0800

    Update acme.sh installation and registration

--- trojan-go_install.sh
+++ trojan-go_install.sh
@@ -235,10 +235,11 @@ tls_generate_script_install() {
     sucess_or_fail "创建acme.sh快捷方式和注册账户"
 
     # 创建快捷命令
-    alias acme.sh=~/.acme.sh/acme.sh
+    export PATH=~/.acme.sh:$PATH
+    chmod +x ~/.acme.sh/acme.sh
     # 注册账号 , 你也可以切换到自己的邮箱， 但是没必要
-    acme.sh --register-account  -m youremail@email.com --server zerossl
     source ~/.bashrc
+    acme.sh --register-account  -m youremail@email.com --server zerossl
 }
 tls_generate() {
   if [[ -f "/data/${domain}/fullchain.crt" ]] && [[ -f "/data/${domain}/privkey.key" ]]; then
