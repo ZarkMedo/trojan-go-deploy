e71d9b6b1493e677409a887a7e7731fd6963429c
Medo <medo@medo.com>
2024-01-15 16:50:26 +0800

    Fix tls_generate_script_install function to register acme account

--- trojan-go_install.sh
+++ trojan-go_install.sh
@@ -232,14 +232,11 @@ tls_generate_script_install() {
     curl https://get.acme.sh | sh
     sucess_or_fail "安装 tls 证书生成脚本"
 
-    sucess_or_fail "创建acme.sh快捷方式和注册账户"
 
-    # 创建快捷命令
-    export PATH=~/.acme.sh:$PATH
-    chmod +x ~/.acme.sh/acme.sh
     # 注册账号 , 你也可以切换到自己的邮箱， 但是没必要
+    "$HOME"/.acme.sh/acme.sh --register-account  -m youremail@email.com --server zerossl
+    sucess_or_fail "注册acme完成"
     source ~/.bashrc
-    acme.sh --register-account  -m youremail@email.com --server zerossl
 }
 tls_generate() {
   if [[ -f "/data/${domain}/fullchain.crt" ]] && [[ -f "/data/${domain}/privkey.key" ]]; then
